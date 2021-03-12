52242002192fe00e0713867dc7377adc8a77a7e7
Jussi Heikkinen <jussih91@gmail.com>
2021-03-12 20:54:39 +0200

    broadcasting, feed etc

--- packages/chat-client/src/App.tsx
+++ packages/chat-client/src/App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react"
+import React, { useEffect, useState, useRef } from 'react'
 import {
   ChakraProvider,
   Box,
@@ -8,31 +8,146 @@ import {
   Code,
   Grid,
   theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+  Button,
+  Flex,
+  Textarea
+} from '@chakra-ui/react'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
+//const ws = new WebSocket('ws://localhost:8080')
+const ws = new WebSocket('ws://localhost:8080')
+
+export const App = () => {
+  const [joined, setJoined] = useState<Boolean>(false)
+  const [input, setInput] = useState<string>('')
+  const [messages, setMessages] = useState<string[]>([])
+  const feedRef = useRef<any>(null)
+
+  useEffect(() => {
+    ws.onopen = () => {
+      console.log('Websocket client connected')
+      setJoined(true)
+    }
+
+    ws.onmessage = (data) => {
+      console.log('received', data)
+      setMessages([...messages, data.data])
+      feedRef.current.scrollIntoView({behavior: 'smooth', block: 'end'})
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [input])
+
+  const submit = () => {
+    if (!joined) return
+
+    console.log(input)
+    ws.send(input)
+    setInput('')
+  }
+
+  const leave = () => {
+    fetch('http://localhost:8080/logout', {
+      method: 'DELETE',
+      //credentials: 'same-origin',
+      credentials: 'include',
+      body: JSON.stringify('masamainio'),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      setJoined(false)
+    })
+    .catch(err => console.error(err))
+  }
+
+  const login = () => {
+    fetch('http://localhost:8080/login', {
+      method: 'POST',
+      //credentials: 'same-origin',
+      credentials: 'include',
+      body: JSON.stringify('masamainio'),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      setJoined(true)
+    })
+    .catch(err => console.error(err))
+  }
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign='center' fontSize='xl'>
+        <Grid minH='100vh' p={3}>
+          <ColorModeSwitcher justifySelf='flex-end' />
+          <Button
+            style={{ position: 'fixed', width: 100 }}
+            onClick={() => !joined ? login() : leave()}
+            colorScheme='blue'
           >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+            {!joined ? 'Join' : 'Leave'}
+          </Button>
+          <Box
+            style={{
+              width: '100%',
+              position: 'fixed',
+              overflowY: 'scroll',
+              bottom: 200,
+              top: 100,
+              right: 10,
+              left: 10
+            }}
+          >
+            <Flex
+              direction={'column'}
+              justify='center'
+              align='center'
+              ref={feedRef}
+            >
+              {messages.map((msg, i) => (
+                <div
+                  key={i}
+                  style={{
+                    width: '90%',
+                    height: '5lh',
+                    background: '#FFF',
+                    borderRadius: 5,
+                    margin: '8px auto'
+                  }}
+                >
+                  <p style={{ color: '#000' }}>{msg}</p>)
+                </div>
+              ))}
+            </Flex>
+          </Box>
+          <Flex
+            direction={'column'}
+            justify='center'
+            align='center'
+            style={{ position: 'fixed', bottom: 20, left: 0, right: 0 }}
+          >
+            <Textarea
+              //onKeyUp={event => event.keyCode === 13}
+              value={input}
+              onChange={event => setInput(event.target.value)}
+              style={{ background: '#FFF', color: '#000', width: '90vw' }}
+              placeholder='Here is a sample placeholder'
+            />
+            <Button
+              onClick={() => submit()}
+              style={{ width: 100, margin: 8 }}
+              >
+              Send
+            </Button>
+          </Flex>
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  )
+}
--- packages/chat-server/express.js
+++ packages/chat-server/express.js
@@ -5,30 +5,23 @@ const express = require('express')
 const http = require('http')
 const uuid = require('uuid')
 const WebSocket = require('ws')
+const cors = require('cors')
 
 const app = express()
 const map = new Map()
 
-//
-// We need the same instance of the session parser in express and
-// WebSocket server.
-//
+app.use(cors({credentials: true, origin: 'http://localhost:3000'}))
+
 const sessionParser = session({
   saveUninitialized: false,
   secret: '$eCuRiTy',
   resave: false
 })
 
-//
-// Serve static files from the 'public' folder.
-//
 app.use(express.static('public'))
 app.use(sessionParser)
 
 app.post('/login', function (req, res) {
-  //
-  // "Log in" user and set userId to session.
-  //
   const id = uuid.v4()
 
   console.log(`Updating session for user ${id}`)
@@ -47,21 +40,16 @@ app.delete('/logout', function (request, response) {
   })
 })
 
-//
-// Create an HTTP server.
-//
 const server = http.createServer(app)
-
-//
-// Create a WebSocket server completely detached from the HTTP server.
-//
 const wss = new WebSocket.Server({ clientTracking: false, noServer: true })
 
 server.on('upgrade', function (request, socket, head) {
   console.log('Parsing session from request...')
 
   sessionParser(request, {}, () => {
+    console.log(request.session)
     if (!request.session.userId) {
+      console.log('no user')
       socket.write('HTTP/1.1 401 Unauthorized\r\n\r\n')
       socket.destroy()
       return
@@ -81,10 +69,14 @@ wss.on('connection', function (ws, request) {
   map.set(userId, ws)
 
   ws.on('message', function (message) {
-    //
-    // Here we can now use session parameters.
-    //
     console.log(`Received message ${message} from user ${userId}`)
+
+    map.forEach((client, key) => {
+      console.log('broadcasting to', key)
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message)
+      }
+    })
   })
 
   ws.on('close', function () {
@@ -92,9 +84,6 @@ wss.on('connection', function (ws, request) {
   })
 })
 
-//
-// Start the server.
-//
 server.listen(8080, function () {
   console.log('Listening on http://localhost:8080')
 })
